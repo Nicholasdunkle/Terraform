56f5a970cd92f460edd7d7122c60277ddbaa91ce
Nicholas Dunkle <nicholas.dunkle@gmail.com>
2025-01-10 10:56:15 -0600

    Create output.tf

---
+++ Newer Projects/Modules/networking-module/vnet/output.tf
@@ -0,0 +1,15 @@
+output "virtual_network_interfaces_ids" {
+    value = azurerm_network_interface.network_interfaces[*].id
+}
+
+output "public_ipaddress" {
+    value = azurerm_public_ip.public_ipaddress[*].ip_address
+}
+
+output "virtual_network_id" {
+    value = azurerm_virtual_network.virtual_network.id 
+}
+
+output "network_interface_private_ip_address" {
+    value = azurerm_network_interface.network_interfaces[*].private_ip_address
+}
