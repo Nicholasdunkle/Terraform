0094c0d461bbf2b108d419dc58ede317fea4158c
Nicholas Dunkle <nicholas.dunkle@gmail.com>
2025-01-10 10:52:19 -0600

    Update main.tf

--- Newer Projects/Modules/main.tf
+++ Newer Projects/Modules/main.tf
@@ -1,3 +1,37 @@
-This project contains a VNet with 2 subnets with NSGs attached. 2 virtual machines in those subnets with nginx installed and a custom Default.html webpage hosted on each one. An Azure Load Balancer with the VMs in the backend pool, a health probe, and load balancer rule. 
+module "resource-group" {
+  source = "./modules/general/resourcegroup"
+  resource_group_name = var.resource_group_name
+  location = var.location
+}
 
-This was practice using modules.
+module "network" {
+  source = "./modules/networking/vnet"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  vnet_name = var.vnet_name
+  vnet_subnet_count = var.vnet_subnet_count
+  vnet_address_prefix = var.vnet_address_prefix
+  depends_on = [ module.resource-group ]
+  network_interface_count = var.network_interface_count
+  public_ip_address_count = var.public_ip_address_count
+  network_security_group_rules = var.network_security_group_rules
+}
+
+module "machines" {
+  source = "./modules/compute/virtualmachines"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  virtual_machine_count = var.virtual_machine_count
+  virtual_network_interface_ids = module.network.virtual_network_interfaces_ids
+  virtual_machine_public_ip_addresses = module.network.public_ipaddress
+}
+
+module "loadbalancer" {
+  source = "./modules/networking/loadbalancer"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  network_interface_private_ip_address = module.network.network_interface_private_ip_address
+  number_of_machines = var.virtual_machine_count
+  virtual_network_id = module.network.virtual_network_id
+  
+}
